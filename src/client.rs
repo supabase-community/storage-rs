f28b2110cc6695567d8aabb9f1b67004fd8e4b8b
Eric Biggs <eric@proziam.com>
2024-10-09 23:35:47 -0400

    Added Bucket Deletion & some cleanup

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,15 @@
-use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::{
+    header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE},
+    StatusCode,
+};
 
 use crate::{
     errors::Error,
-    models::{CreateBucket, CreateBucketOptions, StorageClient, HEADER_API_KEY},
+    models::{CreateBucket, CreateBucketResponse, StorageClient, HEADER_API_KEY, STORAGE_V1},
 };
 
 impl StorageClient {
+    /// Create a new StorageClient from a project_url and api_key
     pub fn new(project_url: String, api_key: String) -> Self {
         Self {
             client: reqwest::Client::new(),
@@ -14,6 +18,8 @@ impl StorageClient {
         }
     }
 
+    /// Create a new StorageClient from the "SUPABASE_URL" and "SUPABASE_API_KEY" environment
+    /// variables.
     pub async fn new_from_env() -> Result<StorageClient, Error> {
         let project_url = std::env::var("SUPABASE_URL")?;
         let api_key = std::env::var("SUPABASE_API_KEY")?;
@@ -23,5 +29,89 @@ impl StorageClient {
             project_url,
             api_key,
         })
+    }
+
+    /// Create a new storage bucket, returning the name of the bucket on success.
+    /// WARNING: Do not use underscores in bucket names or ids
+    pub async fn create_bucket<S: Into<String>>(
+        &self,
+        name: S,
+        id: Option<S>,
+        public: bool,
+        allowed_mime_types: Option<Vec<String>>,
+        file_size_limit: Option<u64>,
+    ) -> Result<String, Error> {
+        let mut headers = HeaderMap::new();
+        headers.insert(HEADER_API_KEY, HeaderValue::from_str(&self.api_key)?);
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("Bearer {}", &self.api_key))?,
+        );
+        headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
+
+        let bucket_name = name.into();
+        let payload = CreateBucket {
+            id: Some(id.map(Into::into).unwrap_or_else(|| bucket_name.clone())),
+            name: bucket_name,
+            public,
+            allowed_mime_types,
+            file_size_limit,
+        };
+
+        let request_body = serde_json::to_string(&payload)?;
+
+        let res = self
+            .client
+            .post(format!("{}{}/bucket", self.project_url, STORAGE_V1))
+            .headers(headers)
+            .body(request_body)
+            .send()
+            .await?;
+
+        let res_status = res.status();
+        let res_body = res.text().await?;
+
+        let bucket = serde_json::from_str::<CreateBucketResponse>(&res_body).map_err(|_| {
+            Error::StorageError {
+                status: res_status,
+                message: res_body,
+            }
+        })?;
+
+        Ok(bucket.name)
+    }
+
+    /// Delete the bucket with the given id
+    pub async fn delete_bucket<S: Into<String>>(&self, id: S) -> Result<(), Error> {
+        let mut headers = HeaderMap::new();
+        headers.insert(HEADER_API_KEY, HeaderValue::from_str(&self.api_key)?);
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("Bearer {}", &self.api_key))?,
+        );
+
+        let res = self
+            .client
+            .delete(format!(
+                "{}{}/bucket/{}",
+                self.project_url,
+                STORAGE_V1,
+                id.into()
+            ))
+            .headers(headers)
+            .send()
+            .await?;
+
+        let res_status = res.status();
+        let res_body = res.text().await?;
+
+        if res_status.is_success() {
+            return Ok(());
+        } else {
+            return Err(Error::StorageError {
+                status: res_status,
+                message: res_body,
+            });
+        }
     }
 }
