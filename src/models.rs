0826ac0dbe300177996fffc7b8de7f73222bd1cc
Eric Biggs <eric@proziam.com>
2024-12-01 16:52:51 -0500

    Documenting TransformOptions

--- src/models.rs
+++ src/models.rs
@@ -197,10 +197,23 @@ pub struct DownloadOptions<'a> {
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct TransformOptions<'a> {
+    /// The width of the image in pixels
     pub width: Option<u64>,
+    /// The height of the image in pixels
     pub height: Option<u64>,
+    /// The resize mode can be cover, contain or fill. Defaults to cover.
+    /// Cover resizes the image to maintain it's aspect ratio while filling the entire width and height.
+    /// Contain resizes the image to maintain it's aspect ratio while fitting the entire image within the width and height.
+    /// Fill resizes the image to fill the entire width and height. If the object's aspect ratio does not match the width and height, the image will be stretched to fit.
     pub resize: Option<&'a str>,
+    /// Specify the format of the image requested.
+    ///
+    /// When using 'origin' we force the format to be the same as the original image.
+    /// When this option is not passed in, images are optimized to modern image formats like Webp.
     pub format: Option<&'a str>,
+    /// Sets the quality of the returned image
+    ///
+    /// A number from 20 to 100, with 100 being the highest quality. Defaults to 80
     pub quality: Option<u8>,
 }
 
